b70267b85784ee89404d65ee1c6cb3d4525f230e
shekabhi369 <shekabhi369@gmail.com>
2019-06-09 12:41:28 +0530

    downloaded from spring initializr

---
+++ src/main/java/com/gfg/shekabhi/springbootwebapplication/SpringbootwebapplicationApplication.java
@@ -0,0 +1,14 @@
+package com.gfg.shekabhi.springbootwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run
+				(SpringbootwebapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfg/shekabhi/springbootwebapplication/SpringbootwebapplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gfg.shekabhi.springbootwebapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootwebapplicationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
